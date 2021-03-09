ce7053cef2e79db84e6b682b59162ba4ad0c8737
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2021-03-09 04:41:37 +0400

    Working additional_buttons

--- app/helpers/tramway/admin/additional_buttons_builder.rb
+++ app/helpers/tramway/admin/additional_buttons_builder.rb
@@ -1,17 +1,10 @@
 # frozen_string_literal: true
 
 module Tramway::Admin::AdditionalButtonsBuilder
-  def build_buttons(additional_buttons)
-    additional_buttons = additional_buttons.is_a?(Hash) ? [additional_buttons] : additional_buttons
-    additional_buttons.each do |button|
-      options = button[:options] || {}
-      concat(
-        link_to(
-          button[:url], method: button[:method], class: "btn btn-#{button[:color]} btn-xs", **options
-        ) do
-          button[:text]
-        end
-      )
-    end
+  def build_button(button)
+    options = button[:options] || {}
+    concat(link_to(button[:url], method: button[:method], class: "btn btn-#{button[:color]} btn-xs", **options) do
+      button[:inner].call
+    end)
   end
 end
