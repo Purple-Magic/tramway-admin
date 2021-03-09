1ab931f67dcb84fd936433f64c3ccba36990d220
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2021-03-09 06:56:45 +0400

    Fix bug with additional_buttons_builder

--- app/helpers/tramway/admin/additional_buttons_builder.rb
+++ app/helpers/tramway/admin/additional_buttons_builder.rb
@@ -4,7 +4,7 @@ module Tramway::Admin::AdditionalButtonsBuilder
   def build_button(button)
     options = button[:options] || {}
     concat(link_to(button[:url], method: button[:method], class: "btn btn-#{button[:color]} btn-xs", **options) do
-      button[:inner].call
+      button[:inner]&.call
     end)
   end
 end
--- lib/tramway/admin/version.rb
+++ lib/tramway/admin/version.rb
@@ -2,6 +2,6 @@
 
 module Tramway
   module Admin
-    VERSION = '2.1'
+    VERSION = '2.1.0.1'
   end
 end
