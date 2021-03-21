dfc99bfcbb16191e4045aa80d4e726d9a5ff7a2b
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2021-03-22 03:15:48 +0400

    Add margin between additional buttons

--- app/helpers/tramway/admin/additional_buttons_builder.rb
+++ app/helpers/tramway/admin/additional_buttons_builder.rb
@@ -3,7 +3,8 @@
 module Tramway::Admin::AdditionalButtonsBuilder
   def build_button(button)
     options = button[:options] || {}
-    concat(link_to(button[:url], method: button[:method], class: "btn btn-#{button[:color]} btn-xs", **options) do
+    style = 'margin-right: 8px'
+    concat(link_to(button[:url], method: button[:method], class: "btn btn-#{button[:color]} btn-xs", style: style, **options) do
       button[:inner]&.call
     end)
   end
